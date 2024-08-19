96d53bd48199dcd1457263d30caabf94f1e4a352
thoriqwildan <wildanthoriq14@gmail.com>
2024-08-19 20:06:50 +0700

    Login-SignUp done

--- account/views.py
+++ account/views.py
@@ -18,5 +18,27 @@ def login_user(request):
         if not user.check_password(request.data['password']):
             return Response({"detail": "Not Found."}, status=status.HTTP_404_NOT_FOUND)
         token, created = Token.objects.get_or_create(user=user)
-        serializer = UserSerializer(instance=User)
+        serializer = UserSerializer(instance=user)
         return Response({"token": token.key, "user": serializer.data})
+    
+@api_view(['POST', 'GET'])
+def signup_user(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        user = User.objects.get(username=request.data['username'])
+        user.set_password(request.data['password'])
+        user.save()
+        token = Token.objects.create(user=user)
+        return Response({"token": token.key, "user": serializer.data})
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+from rest_framework.decorators import authentication_classes, permission_classes
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+@api_view(['GET'])
+@authentication_classes([SessionAuthentication, TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def test_token(request):
+    return Response("passed for {}".format(request.user.email))
